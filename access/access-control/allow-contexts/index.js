fec6e480af513cb3a9bc95b8d6ce525bf71f955d
SorinGFS <sorin.gfs@protonmail.com>
2021-12-21 18:23:02 +0200

    fix missing return

--- access/access-control/allow-contexts/index.js
+++ access/access-control/allow-contexts/index.js
@@ -4,7 +4,7 @@ const createError = require('http-errors');
 // allow contexts
 const allowContexts = (req, res, next) => {
     if (req.server.accessControl && req.server.accessControl.allowContexts) {
-        req.server.fn.hasExactContextMatches(req, req.server.accessControl.allowContexts) ? next() : next(createError.Forbidden());
+        return req.server.fn.hasExactContextMatches(req, req.server.accessControl.allowContexts) ? next() : next(createError.Forbidden());
     }
     next();
 }
