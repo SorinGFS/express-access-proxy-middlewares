348e55722ea7a9e13a31098e0753470d2edcc1b7
SorinGFS <sorin.gfs@protonmail.com>
2021-12-22 17:07:21 +0200

    improvement accessLogs and errorLogs

--- http-errors/index.js
+++ http-errors/index.js
@@ -1,15 +1,17 @@
 'use strict';
 // this is a custom handler for errors created with http-errors plus internal errors
-const handleError = (err, req, res, next) => {
+const handleError = async (err, req, res, next) => {
     const { statusCode = 500, expose, message, stack, headers, ...rest } = err;
     // log server errors (app errors are NOT logged into db, you may implement your own solution here)
     if (req.server && req.server.errorLogs) {
+        // cancel access log to avoid redundant info for the same request
+        req.removeAllListeners('end');
         const log = {};
         if (Array.isArray(req.server.errorLogs)) {
             req.server.errorLogs.forEach((item) => (log[item] = req[item]));
-            req.accessDb.controller('errors').insertOne({ time: new Date(), ...log });
+            await req.accessDb.controller('errors').insertOne({ time: new Date(), app: process.env.APP_NAME, message: message, ...log });
         } else {
-            req.accessDb.controller('errors').insertOne({ time: new Date(), ip: req.ip, ips: req.ips, method: req.method, protocol: req.protocol, hostname: req.hostname, url: req.url, message: message });
+            await req.accessDb.controller('errors').insertOne({ time: new Date(), app: process.env.APP_NAME, message: message, ip: req.ip, ips: req.ips, method: req.method, protocol: req.protocol, hostname: req.hostname, url: req.url });
         }
     }
     if (headers) res.set(headers);
