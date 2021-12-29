de9b756ca0b359e1e357c77cda2ac77a8f48ae04
SorinGFS <sorin.gfs@protonmail.com>
2021-12-30 00:24:44 +0200

    bump express-access-proxy-base to 1.5.2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // dynamically export the folders and nodejs supported files in current working dir
 const fs = require('express-access-proxy-base/fs');
 const dirs = fs.dirs(__dirname);
-const files = fs.files(__dirname);
+const entries = fs.entries(__dirname);
 
 const fileName = (file) => {
     file = file.split('.');
@@ -13,7 +13,7 @@ const fileName = (file) => {
 dirs.forEach((dir) => {
     Object.assign(module.exports, { [dir]: require(fs.pathResolve(__dirname, dir)) });
 });
-files.forEach((file) => {
+entries.forEach((file) => {
     if (['js', 'mjs', 'json'].includes(file.split('.').pop())) {
         Object.assign(module.exports, { [fileName(file)]: require(fs.pathResolve(__dirname, file)) });
     }
